f76a1e0d608fa6be0cf4b4c1b89c7205bb4563c2
Milk <145270243+Milky47Way@users.noreply.github.com>
2026-01-12 19:36:28 +0200

    Add files via upload

---
+++ school/education/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Group(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+#class Subject(models.Model):
+#name = models.CharField(max_length=100)
